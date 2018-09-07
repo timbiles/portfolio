1edaa875fdcfaaa672df8dc0dc050cc25ca691ed
Tim Biles <timbilestimbiles@gmail.com>
2018-09-07 16:44:22 -0500

    pushing before host

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 
 import convoke from '../components/photos/convoke.jpg'
 import region20 from '../components/photos/Region20.jpg'
+import dad from '../components/photos/dad-portfolio.jpg'
 
 const Portfolio = () => (
   <div className="about_main">
@@ -28,6 +29,14 @@ const Portfolio = () => (
         </a>
         <div className='port_hidden'>
           <h2>Region 20</h2>
+        </div>
+      </div>
+      <div className='port_sub_container'>
+        <a href="http://204.48.21.195:8081/" target="blank">
+          <img className="portfolio_img" src={dad} alt="DeronBiles.com" />
+        </a>
+        <div className='port_hidden'>
+          <h2>Deron Biles</h2>
         </div>
       </div>
 
