3a0f8d30a21ef9c65bf5ea869e8672d1437f9ffd
Tim Biles <timbilestimbiles@gmail.com>
2018-09-27 14:28:53 -0500

    random

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <div className="main_container">
     <div className="main_sub">
       <div className="main_title1">
-        <h1 className="main_text">Tim Biles</h1>
+        <h1 className="main_text">Bob Biles</h1>
         <img className='index_img' src="https://image.flaticon.com/icons/svg/587/587376.svg" alt="" />
       </div>
       <h3 className="main_sub_text">Web Developer</h3>
